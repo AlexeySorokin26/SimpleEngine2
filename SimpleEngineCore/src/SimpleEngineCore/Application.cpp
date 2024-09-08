136af81c05218fee2bd8a9dab2d5f2de0e63938e
Alexey Sorokin <asorokin@gremse-it.com>
2024-09-08 23:59:45 +0200

    app class

--- SimpleEngineCore/src/SimpleEngineCore/Application.cpp
+++ SimpleEngineCore/src/SimpleEngineCore/Application.cpp
@@ -1,9 +1,17 @@
+#include "SimpleEngineCore/Application.h"
 #include <iostream>
-#include "SimpleEngineCore/Utils/test.h"
 #include <GLFW/glfw3.h>
 
 namespace SimpleEngine {
-	int checkGLFW() {
+	Application::Application() {
+
+	}
+	
+	Application::~Application() {
+
+	}
+
+	int Application::start(unsigned int window_width, unsigned int window_heith, const char* title) {
 		GLFWwindow* window;
 
 		/* Initialize the library */
@@ -11,7 +19,7 @@ namespace SimpleEngine {
 			return -1;
 
 		/* Create a windowed mode window and its OpenGL context */
-		window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+		window = glfwCreateWindow(window_width, window_heith, title, NULL, NULL);
 		if (!window)
 		{
 			glfwTerminate();
@@ -32,9 +40,15 @@ namespace SimpleEngine {
 
 			/* Poll for and process events */
 			glfwPollEvents();
+
+			on_update();
 		}
 
 		glfwTerminate();
 		return 0;
+	}
+	
+	void Application::on_update() {
+
 	}
 }
--- SimpleEngineEditor/src/main.cpp
+++ SimpleEngineEditor/src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-#include <SimpleEngineCore/Utils/test.h>
+#include <memory>
+#include "SimpleEngineCore/Application.h"
+
+class MyApp : public SimpleEngine::Application {
+	virtual void on_update() override {
+		std::cout << "Update frame: " << frame++ << std::endl;
+	}
+
+	int frame = 0;
+};
+
 
 int main() {
-	SimpleEngine::checkGLFW();
+	auto myApp = std::make_unique<MyApp>();
+
+	int returnCode = myApp->start(1024, 768, "My first app");
+
+	return returnCode;
 }
