608758a1a6446d9b65d53f1c244a00168893314c
Alexey Sorokin <asorokin@gremse-it.com>
2024-11-26 23:17:30 +0100

    dir light

--- SimpleEngineCore/shaders/phong_cube_fragment_shader.glsl
+++ SimpleEngineCore/shaders/phong_cube_fragment_shader.glsl
@@ -53,60 +53,60 @@ out vec4 frag_color;
 
 // Function to compute point light contribution
 vec3 ComputePointLight(vec3 frag_pos, vec3 frag_normal, vec3 view_direction) {
-    // Distance and attenuation
-    float distance = length(pointLight.position - frag_pos);
-    float attenuation = 1.0 / (pointLight.constant +
-        pointLight.linear * distance +
-        pointLight.quadratic * (distance * distance));
+	// Distance and attenuation
+	float distance = length(pointLight.position - frag_pos);
+	float attenuation = 1.0 / (pointLight.constant +
+		pointLight.linear * distance +
+		pointLight.quadratic * (distance * distance));
 
-    // Ambient
-    vec3 ambient = attenuation * pointLight.ambientIntensity * pointLight.ambient * material.ambient;
+	// Ambient
+	vec3 ambient = attenuation * pointLight.ambientIntensity * pointLight.ambient * material.ambient;
 
-    // Diffuse
-    vec3 light_direction = normalize(pointLight.position - frag_pos);
-    vec3 diffuse = attenuation * pointLight.diffuseIntensity * pointLight.diffuse * material.diffuse *
-        max(dot(light_direction, frag_normal), 0.0);
+	// Diffuse
+	vec3 light_direction = normalize(pointLight.position - frag_pos);
+	vec3 diffuse = attenuation * pointLight.diffuseIntensity * pointLight.diffuse * material.diffuse *
+		max(dot(light_direction, frag_normal), 0.0);
 
-    // Specular
-    vec3 reflected_direction = reflect(-light_direction, frag_normal);
-    vec3 specular = attenuation * pointLight.specularIntensity * pointLight.specular * material.specular *
-        pow(max(dot(reflected_direction, view_direction), 0.0), material.shininess);
+	// Specular
+	vec3 reflected_direction = reflect(-light_direction, frag_normal);
+	vec3 specular = attenuation * pointLight.specularIntensity * pointLight.specular * material.specular *
+		pow(max(dot(reflected_direction, view_direction), 0.0), material.shininess);
 
-    return ambient + diffuse + specular;
+	return ambient + diffuse + specular;
 }
 
 // Function to compute directional light contribution
 vec3 ComputeDirectionalLight(vec3 frag_normal, vec3 view_direction) {
-    // Ambient
-    vec3 ambient = directionalLight.ambientIntensity * directionalLight.ambient * material.ambient;
+	// Ambient
+	vec3 ambient = directionalLight.ambientIntensity * directionalLight.ambient * material.ambient;
 
-    // Diffuse
-    vec3 light_direction = normalize(-directionalLight.direction);
-    vec3 diffuse = directionalLight.diffuseIntensity * directionalLight.diffuse * material.diffuse *
-        max(dot(light_direction, frag_normal), 0.0);
+	// Diffuse
+	vec3 light_direction = normalize(-directionalLight.direction);
+	vec3 diffuse = directionalLight.diffuseIntensity * directionalLight.diffuse * material.diffuse *
+		max(dot(light_direction, frag_normal), 0.0);
 
-    // Specular
-    vec3 reflected_direction = reflect(-light_direction, frag_normal);
-    vec3 specular = directionalLight.specularIntensity * directionalLight.specular * material.specular *
-        pow(max(dot(reflected_direction, view_direction), 0.0), material.shininess);
+	// Specular
+	vec3 reflected_direction = reflect(-light_direction, frag_normal);
+	vec3 specular = directionalLight.specularIntensity * directionalLight.specular * material.specular *
+		pow(max(dot(reflected_direction, view_direction), 0.0), material.shininess);
 
-    return ambient + diffuse + specular;
+	return ambient + diffuse + specular;
 }
 
 void main() {
-    // Normalize inputs
-    vec3 normal = normalize(frag_normal);
-    vec3 view_direction = normalize(cam_pos - frag_pos);
+	// Normalize inputs
+	vec3 normal = normalize(frag_normal);
+	vec3 view_direction = normalize(cam_pos - frag_pos);
 
-    // Compute lighting
-    vec3 point_light = ComputePointLight(frag_pos, normal, view_direction);
-    vec3 directional_light = ComputeDirectionalLight(normal, view_direction);
+	// Compute lighting
+	vec3 point_light = ComputePointLight(frag_pos, normal, view_direction);
+	vec3 directional_light = ComputeDirectionalLight(normal, view_direction);
 
-    // Combine lights
-    vec3 total_light = globalAmbient * material.ambient + point_light /*+ directional_light*/;
+	// Combine lights
+	vec3 total_light = globalAmbient * material.ambient + point_light + directional_light;
 
-    // Texture mapping and output
-    vec4 colorTex = texture(InTexture, tex_coord);
-    vec4 colorTex1 = texture(InTexture1, tex_coord);
-    frag_color = vec4(total_light, 1.0) * mix(colorTex, colorTex1, 0.5);
+	// Texture mapping and output
+	vec4 colorTex = texture(InTexture, tex_coord);
+	vec4 colorTex1 = texture(InTexture1, tex_coord);
+	frag_color = vec4(total_light, 1.0) * mix(colorTex, colorTex1, 0.5);
 }
