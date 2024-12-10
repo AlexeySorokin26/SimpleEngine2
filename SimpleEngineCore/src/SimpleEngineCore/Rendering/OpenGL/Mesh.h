50d2928f51f4097fc9eb8aa1428d2c9f0806ddef
Alexey Sorokin <asorokin@gremse-it.com>
2024-12-10 22:20:50 +0100

    blender

--- SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/Mesh.h
+++ SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/Mesh.h
@@ -19,6 +19,7 @@
 #include "SimpleEngineCore/Rendering/OpenGL/Renderer_OpenGL.h"
 #include "SimpleEngineCore/Camera.h"
 #include "SimpleEngineCore/Utils.h"
+#include "SimpleEngineCore/Log.h"
 
 #include <GLFW/glfw3.h>
 
@@ -40,11 +41,11 @@ namespace SimpleEngine {
 			: std::runtime_error(message) {}
 	};
 
-	//struct Vertex {
-	//	glm::vec3 Position;
-	//	glm::vec3 Normal;
-	//	glm::vec2 TexCoords;
-	//};
+	struct Vertex {
+		glm::vec3 Position;
+		glm::vec3 Normal;
+		glm::vec2 TexCoords;
+	};
 
 	class Mesh {
 	public:
@@ -52,7 +53,8 @@ namespace SimpleEngine {
 			const Camera& camera,
 			std::vector<GLfloat> vertices,
 			std::vector<GLuint> indices,
-			std::filesystem::path vertex_shader_path, std::filesystem::path frag_shader_path,
+			std::filesystem::path vertex_shader_path,
+			std::filesystem::path frag_shader_path,
 			std::vector<std::filesystem::path> v_texturePaths = {}) :
 			camera(camera),
 			vertices(vertices), indices(indices)/*, m_texture(m_texture)*/
@@ -68,7 +70,8 @@ namespace SimpleEngine {
 			}
 			SetupMesh();
 		}
-		virtual void Draw() = 0;
+		virtual void Draw() {
+		}
 
 		void UpdateCamera(const Camera& cam) {
 			this->camera = cam;
@@ -120,17 +123,97 @@ namespace SimpleEngine {
 		std::string directory;
 
 		void LoadModel(std::string path) {
+			Assimp::Importer import;
+			const aiScene* scene = import.ReadFile(path,
+				aiProcess_Triangulate | aiProcess_FlipUVs);
 
+			if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE ||
+				!scene->mRootNode)
+			{
+				LOG_INFO("ERROR::ASSIMP::{0}", import.GetErrorString());
+				return;
+			}
+			directory = path.substr(0, path.find_last_of('/'));
+			ProcessNode(scene->mRootNode, scene);
 		}
 		void ProcessNode(aiNode* node, const aiScene* scene) {
-
+			// process all the node’s meshes (if any)
+			for (unsigned int i = 0; i < node->mNumMeshes; i++)
+			{
+				aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+				meshes.push_back(ProcessMesh(mesh, scene));
+			}
+			// then do the same for each of its children
+			for (unsigned int i = 0; i < node->mNumChildren; i++)
+			{
+				ProcessNode(node->mChildren[i], scene);
+			}
 		}
-		/*Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene) {
-
-		}*/
+		Mesh ProcessMesh(aiMesh* mesh, const aiScene* scene) {
+			std::vector<Vertex> vertices;
+			std::vector<unsigned int> indices;
+			std::vector<Texture2D> textures;
+			for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+			{
+				Vertex vertex;
+				// process vertex positions, normals and texture coordinates
+				// Vertices
+				glm::vec3 vector;
+				vector.x = mesh->mVertices[i].x;
+				vector.y = mesh->mVertices[i].y;
+				vector.z = mesh->mVertices[i].z;
+				vertex.Position = vector;
+				// Normals
+				vector.x = mesh->mNormals[i].x;
+				vector.y = mesh->mNormals[i].y;
+				vector.z = mesh->mNormals[i].z;
+				vertex.Normal = vector;
+				// Textures
+				if (mesh->mTextureCoords[0])
+				{
+					glm::vec2 vec;
+					vec.x = mesh->mTextureCoords[0][i].x;
+					vec.y = mesh->mTextureCoords[0][i].y;
+					vertex.TexCoords = vec;
+				}
+				else
+					vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+				vertices.push_back(vertex);
+			}
+			// process indices
+			for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+			{
+				aiFace face = mesh->mFaces[i];
+				for (unsigned int j = 0; j < face.mNumIndices; j++)
+					indices.push_back(face.mIndices[j]);
+			}
+			// process material
+			if (mesh->mMaterialIndex >= 0)
+			{
+				aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+				std::vector<Texture2D> diffuseMaps = LoadMaterialTextures(material,
+					aiTextureType_DIFFUSE, "texture_diffuse");
+				textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+				std::vector<Texture2D> specularMaps = LoadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+				textures.insert(textures.end(), specularMaps.begin(),
+					specularMaps.end());
+			}
+			return Mesh(vertices, indices, textures);
+		}
 		std::vector<Texture2D> LoadMaterialTextures(aiMaterial* mat,
 			aiTextureType type, std::string typeName) {
-
+			std::vector<Texture2D> textures;
+			for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+			{
+				aiString str;
+				mat->GetTexture(type, i, &str);
+				Texture2D texture;
+				texture.id = TextureFromFile(str.C_Str(), directory);
+				texture.type = typeName;
+				texture.path = str;
+				textures.push_back(texture);
+			}
+			return textures;
 		}
 	};
 
